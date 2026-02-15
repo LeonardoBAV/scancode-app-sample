f5eb25a4da1e7621097ff6c17ec72f3e175bf82e
Leonardo Borges Athayde Vasconcelos <leobav1988@gmail.com>
2026-02-14 22:01:57 -0300

    init

---
+++ app/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <Page>
+    <ActionBar>
+      <Label text="Home" class="font-bold text-lg"/>
+    </ActionBar>
+
+    <GridLayout>
+      <Label class="text-xl align-middle text-center text-gray-500" :text="message" @tap="logMessage" />
+    </GridLayout>
+  </Page>
+</template>
+
+<script lang="ts">
+  export default {
+    computed: {
+      message() {
+        return "Blank {N}-Vue app";
+      }
+    },
+
+    methods: {
+      logMessage() {
+        console.log('You have tapped the message!')
+      }
+    }
+  };
+</script>
